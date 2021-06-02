6c91e881ce27cb252076c4cb302b33486aa8bdcb
Tiffany Zhao <tiffanyzhao@Tiffanys-MBP-2.attlocal.net>
2021-06-01 22:24:38 -0700

    Weights for NN results

--- eigen-portfolio/PCA.py
+++ eigen-portfolio/PCA.py
@@ -5,6 +5,84 @@ import datetime as dt
 import matplotlib.pyplot as plt
 
 from sklearn.decomposition import PCA
-from datasets import shift
+from pandas_datareader import data as pdr
+import yfinance as yf
 
+yf.pdr_override()
+stocks = "AAPL MSFT AMZN GOOGL FB TSLA BRK-A JPM V JNJ"
+
+df = pdr.get_data_yahoo(tickers=stocks, start="2014-01-01", end="2021-05-29")['Adj Close']
+returns = df.pct_change().dropna()
+cov = df.cov()
+pca = PCA(n_components=2)
+components = pca.fit_transform(returns)
+
+
+pca = PCA()
+pca.fit(cov)
+
+def plotPCA(plot=False):
+
+    # Visualizing Variance against number of principal components.
+
+    var_threshold = 0.95
+    var_explained = np.cumsum(pca.explained_variance_ratio_)
+    num_comp = np.where(np.logical_not(var_explained < var_threshold))[0][0] + 1  
+
+    if plot:
+        print('%d principal components explain %.2f%% of variance' %(num_comp, 100* var_threshold))
+
+        # PCA percent variance explained.
+        bar_width = 0.9
+        n_asset = len(stocks)
+        x_indx = np.arange(n_asset)
+        fig, ax = plt.subplots()
+
+        # Eigenvalues measured as percentage of explained variance.
+        rects = ax.bar(x_indx, pca.explained_variance_ratio_[:n_asset], bar_width)
+        ax.set_xticks(x_indx + bar_width / 2)
+        ax.set_xticklabels(list(range(n_asset)), rotation=45)
+        ax.set_title('Percent variance explained')
+        ax.set_ylabel('Explained Variance')
+        ax.set_xlabel('Principal Components')
+        plt.show()
+
+plotPCA(plot=True)
+
+projected = pca.fit_transform(cov)
+pcs = pca.components_
+
+def PCWeights():
+    '''
+    Principal Components (PC) weights for each 28 PCs
+    '''
+    weights = pd.DataFrame()
+
+    for i in range(len(pcs)):
+        weights["weights_{}".format(i)] = pcs[:, i] / sum(pcs[:, i])
+
+    weights = weights.values.T
+    return weights
+
+weights = PCWeights()
+portfolio = portfolio = pd.DataFrame()
+
+def plotEigen(weights, plot=False, portfolio=portfolio):
+    portfolio = pd.DataFrame(data ={'weights': weights.squeeze()*100}, index = stocks) 
+    portfolio.sort_values(by=['weights'], ascending=False, inplace=True)
+    
+    if plot:
+        print('Sum of weights of current eigen-portfolio: %.2f' % np.sum(portfolio))
+        portfolio.plot(title='Current Eigen-Portfolio Weights', 
+            figsize=(12,6), 
+            xticks=range(0, len(stocks),1), 
+            rot=45, 
+            linewidth=3
+            )
+        plt.show()
+
+    return portfolio
+
+# Weights are stored in arrays, where 0 is the first PC's weights.
+plotEigen(weights=weights[4], plot=True)
 
--- stock_portfolio/efficient_frontier.py
+++ stock_portfolio/efficient_frontier.py
@@ -5,6 +5,8 @@ from pypfopt import risk_models
 from pypfopt import expected_returns
 from pypfopt.efficient_frontier import EfficientFrontier
 from pypfopt import plotting
+from pypfopt import objective_functions
+from pypfopt.discrete_allocation import DiscreteAllocation, get_latest_prices
 from pandas_datareader import data as pdr
 import yfinance as yf
 
@@ -13,7 +15,7 @@ import pandas as pd
 stocks = "AAPL MSFT AMZN GOOGL FB TSLA BRK-A JPM V JNJ"
 
 
-def plot_ef(ef):
+def plot_ef(ef, method=None):
     fig, ax = plt.subplots()
     plotting.plot_efficient_frontier(ef, ax=ax)
     ef.add_constraint(lambda w: w[0]+w[1]+w[2]+w[3]+w[4]+w[5]+w[6]+w[7]+w[8]+w[9] == 1)
@@ -23,7 +25,7 @@ def plot_ef(ef):
     optimal, volatility, _ = ef.portfolio_performance(verbose=True)
     ax.plot(volatility, optimal, marker="*", label="optimal")
     ax.legend()
-    plt.savefig("ef.png")
+    plt.savefig("ef_{}.png".format(method))
     plt.show()
 
 def plot_covariance(S):
@@ -56,12 +58,27 @@ def random_portfolios(mu, S):
     plt.tight_layout()
     plt.savefig("ef_scatter.png", dpi=200)
     plt.show()
+#How many shares of each asset you should purchase
+def allocate(df, ef):
+    ef.add_objective(objective_functions.L2_reg, gamma=0.1)
+    w = ef.max_sharpe()
+    latest_prices = get_latest_prices(df)
+    da = DiscreteAllocation(w, latest_prices, total_portfolio_value=20000)
+    allocation, leftover = da.lp_portfolio()
+    print(allocation)
 
-df = pdr.get_data_yahoo(tickers=stocks, start="2014-01-01", end="2021-05-29")['Adj Close']
-returns = df.pct_change().dropna()
-mu = expected_returns.mean_historical_return(df)
-S = risk_models.sample_cov(df)
-ef = EfficientFrontier(mu, S)
-plot_ef(ef)
-plot_covariance(S)
-random_portfolios(mu,S)
+def run(method):
+    if method == "NN":
+        df = pd.read_pickle("../NN/expected_returns_NN.pkl")
+    else:
+        #2019-12-12 to now
+        df = pdr.get_data_yahoo(tickers=stocks, start="2014-01-01", end="2021-05-29")['Adj Close']
+        returns = df.pct_change().dropna()
+    mu = expected_returns.mean_historical_return(df)
+    S = risk_models.sample_cov(df)
+    ef = EfficientFrontier(mu, S)
+    plot_ef(ef, method=method)
+# plot_covariance(S)
+# random_portfolios(mu,S)
+if __name__ == '__main__':
+    run("NN")
